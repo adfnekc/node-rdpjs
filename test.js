27be8e9970fb65fa5d93ff2cb2920925e16e04ad
adfnekc <i286@foxmail.com>
2021-07-15 10:50:46 +0800

    for test

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const scrpy_rdp = require("./scrpy");
 const analysisLog = require('why-is-node-running');
+const { getSystemErrorMap } = require("util");
 
 (async () => {
     let ip = "192.168.31.8";
+    if (process.argv[2]){
+        ip = process.argv[2]
+    }
     let r = await scrpy_rdp(ip);
     console.log(r);
     //analysisLog();
