3bf8b277a76a110f16a70fb3df4f973cfbe56ba3
adfnekc <i286@foxmail.com>
2021-07-15 10:54:56 +0800

    for test

--- test.js
+++ test.js
@@ -4,10 +4,14 @@ const { getSystemErrorMap } = require("util");
 
 (async () => {
     let ip = "192.168.31.8";
-    if (process.argv[2]){
+    let port = 3389
+    if (process.argv[2]) {
         ip = process.argv[2]
     }
-    let r = await scrpy_rdp(ip);
+    if (process.argv[3]) {
+        port = process.argv[3]
+    }
+    let r = await scrpy_rdp(ip, port);
     console.log(r);
     //analysisLog();
 })()
